7035867f3e96946dbede5512dfe208b7e8d825e7
wayaki <wayaki.sushibake@gmail.com>
2026-09-09 01:43:08 +0800

    Update receipt.js

--- js/checkout/receipt.js
+++ js/checkout/receipt.js
@@ -510,7 +510,7 @@ function renderSelections(
                     " "
                   )
                 )
-              : `Option ${groupNumber}`;
+              : "";
 
 
           // ========================
